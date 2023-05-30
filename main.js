588c66a11cc4e823e457b8c528606cfaf483887c
CodingWithAbdou <coder.abdou@gmail.com>
2023-05-30 17:43:03 +0100

    Upload the profile

---
+++ main.js
@@ -0,0 +1,582 @@
+// data of Project
+
+const dataProject = [
+  {
+    TypeProject: "mini-project",
+    rootImage: "./images/pro21.PNG",
+    altImage: "Project Twentyone",
+    titleProject: "Weather App",
+    aboutProject: "Web Development",
+    hostProject: "https://subtle-tapioca-5cad49.netlify.app/",
+    nameHost: "https://subtle-tapioca-5cad49.netlify.app/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: [
+      "Vue js",
+      "Vue Router",
+      "javaScript",
+      "CSS",
+      "HTMl",
+      "axias",
+    ],
+  },
+
+  {
+    TypeProject: "template",
+    rootImage: "./images/pro1.PNG",
+    altImage: "Project One",
+    titleProject: "bondi",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/bondi/",
+    nameHost: "https://codingwithabdou.github.io/bondi/",
+    buildTime: { year: 2022, month: 08, day: 19 },
+    projectLanguages: ["HTML", "CSS", "javaScript", "Bootstrap5"],
+  },
+  {
+    TypeProject: "template",
+    rootImage: "./images/pro3.PNG",
+    altImage: "Project Three",
+    titleProject: "Tech Educa",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/education/",
+    nameHost: "https://codingwithabdou.github.io/education/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "SASS", "javaScript"],
+  },
+  {
+    TypeProject: "template",
+    rootImage: "./images/pro4.PNG",
+    altImage: "Project Four",
+    titleProject: "Hospital",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/Hospital/",
+    nameHost: "https://codingwithabdou.github.io/Hospital/",
+    buildTime: { year: 2022, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript", "SASS"],
+  },
+  {
+    TypeProject: "template",
+    rootImage: "./images/pro5.PNG",
+    altImage: "Project Five",
+    titleProject: "Dachboard",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/Dachboard/",
+    nameHost: "https://codingwithabdou.github.io/Dachboard/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS"],
+  },
+  {
+    TypeProject: "template",
+    rootImage: "./images/pro6.PNG",
+    altImage: "Project Six",
+    titleProject: "mixanTemplate",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/mixanTemplate/",
+    nameHost: "https://codingwithabdou.github.io/mixanTemplate/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript"],
+  },
+  {
+    TypeProject: "template",
+    rootImage: "./images/pro7.PNG",
+    altImage: "Project Seven",
+    titleProject: "Kasper",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/kasper/",
+    nameHost: "https://codingwithabdou.github.io/kasper/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript"],
+  },
+  {
+    TypeProject: "template",
+    rootImage: "./images/pro2.PNG",
+    altImage: "Project Two",
+    titleProject: "Bookish",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/bookish/",
+    nameHost: "https://codingwithabdou.github.io/bookish/",
+    buildTime: { year: 2023, month: 02, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript"],
+  },
+
+  {
+    TypeProject: "template",
+    rootImage: "./images/pro8.PNG",
+    altImage: "Project Eight",
+    titleProject: "Composition",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/Composotion/",
+    nameHost: "https://codingwithabdou.github.io/Composotion/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "SASS", "javaScript"],
+  },
+  {
+    TypeProject: "template",
+    rootImage: "./images/pro9.PNG",
+    altImage: "Project Nine",
+    titleProject: "Agency",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/awosem-Agency/",
+    nameHost: "https://codingwithabdou.github.io/awosem-Agency/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript"],
+  },
+
+  {
+    TypeProject: "other",
+    rootImage: "./images/pro10.PNG",
+    altImage: "Project Ten",
+    titleProject: "Slider__Responsive",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/Slider__Responsive/",
+    nameHost: "https://codingwithabdou.github.io/Slider__Responsive/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript"],
+  },
+  {
+    TypeProject: "game",
+    rootImage: "./images/pro11.PNG",
+    altImage: "Project eleven",
+    titleProject: "Hangeman",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/education/",
+    nameHost: "https://codingwithabdou.github.io/education/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript", "SASS"],
+  },
+  {
+    TypeProject: "other",
+    rootImage: "./images/pro12.PNG",
+    altImage: "Project Twelve",
+    titleProject: "Nice_Effect",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/education/",
+    nameHost: "https://codingwithabdou.github.io/education/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript", "SASS"],
+  },
+  {
+    TypeProject: "mini-project",
+    rootImage: "./images/pro13.PNG",
+    altImage: "Project Thirteen",
+    titleProject: "Sounds_player",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/education/",
+    nameHost: "https://codingwithabdou.github.io/education/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript", "SASS"],
+  },
+  {
+    TypeProject: "mini-project",
+    rootImage: "./images/pro14.PNG",
+    altImage: "Project fourteenth",
+    titleProject: "Nice_image_slider",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/education/",
+    nameHost: "https://codingwithabdou.github.io/education/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript", "SASS"],
+  },
+  {
+    TypeProject: "game",
+    rootImage: "./images/pro15.PNG",
+    altImage: "Project fifteenth",
+    titleProject: "Speed_typing",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/education/",
+    nameHost: "https://codingwithabdou.github.io/education/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript", "SASS"],
+  },
+  {
+    TypeProject: "min-project",
+    rootImage: "./images/pro16.PNG",
+    altImage: "Project sixteenth",
+    titleProject: "Simple_toDo",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/education/",
+    nameHost: "https://codingwithabdou.github.io/education/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript", "SASS"],
+  },
+
+  {
+    TypeProject: "mini-project",
+    rootImage: "./images/pro17.PNG",
+    altImage: "Project seventeenth",
+    titleProject: "quiz",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/education/",
+    nameHost: "https://codingwithabdou.github.io/education/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript", "SASS"],
+  },
+  {
+    TypeProject: "mini-project",
+    rootImage: "./images/pro18.PNG",
+    altImage: "Project Eighteen ",
+    titleProject: "GetRepo",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/education/",
+    nameHost: "https://codingwithabdou.github.io/education/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript", "SASS"],
+  },
+  {
+    TypeProject: "other",
+    rootImage: "./images/pro19.PNG",
+    altImage: "Project Nineteen",
+    titleProject: "listOpen",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/education/",
+    nameHost: "https://codingwithabdou.github.io/education/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript", "SASS"],
+  },
+  {
+    TypeProject: "game",
+    rootImage: "./images/pro20.PNG",
+    altImage: "Project Twenty",
+    titleProject: "memoryGame",
+    aboutProject: "Web Development",
+    hostProject: "https://codingwithabdou.github.io/education/",
+    nameHost: "https://codingwithabdou.github.io/education/",
+    buildTime: { year: 2021, month: 12, day: 22 },
+    projectLanguages: ["HTML", "CSS", "javaScript", "SASS"],
+  },
+];
+
+const projectList = document.querySelector(".project__list");
+
+// handel  data and add them in template
+
+for (let i = 0, len = dataProject.length; i < len; i++) {
+  const li = document.createElement("li");
+  li.className = "project__item active";
+  li.setAttribute("data-type", dataProject[i].TypeProject);
+
+  const div = document.createElement("div");
+  li.append(div);
+
+  const figure = document.createElement("figure");
+  figure.className = "project-img f-centering";
+  div.append(figure);
+
+  const divProjectItem = document.createElement("div");
+  divProjectItem.className = "project-item";
+  figure.append(divProjectItem);
+
+  const buttonAbout = document.createElement("button");
+  buttonAbout.className = "btn f-centering showbigger";
+  buttonAbout.innerText = "About Project";
+  divProjectItem.append(buttonAbout);
+
+  const imgTag = document.createElement("img");
+  imgTag.className = "img";
+  imgTag.src = dataProject[i].rootImage;
+  imgTag.alt = dataProject[i].altImage;
+  imgTag.setAttribute("data-project-image", "");
+  imgTag.setAttribute("landing", "lazy");
+  imgTag.setAttribute("width", 100);
+  figure.append(imgTag);
+
+  const headTag = document.createElement("h3");
+  headTag.className = "project-title";
+  headTag.setAttribute("data-project-title", "");
+  headTag.innerText = dataProject[i].titleProject;
+  div.append(headTag);
+
+  const pargraphTag = document.createElement("p");
+  pargraphTag.className = "project-about";
+  pargraphTag.setAttribute("data-project-about", "");
+  pargraphTag.innerText = dataProject[i].aboutProject;
+  div.append(pargraphTag);
+
+  const divMorPro = document.createElement("div");
+  divMorPro.className = "project__more";
+  div.append(divMorPro);
+
+  const linkTag = document.createElement("a");
+  linkTag.href = dataProject[i].hostProject;
+  linkTag.setAttribute("data-project-link", "");
+  linkTag.innerText = dataProject[i].nameHost;
+  divMorPro.append(linkTag);
+
+  const timeTag = document.createElement("time");
+  timeTag.setAttribute("data-project-time", "");
+  timeTag.dateTime = `${dataProject[i].buildTime.year}-${dataProject[i].buildTime.month}-${dataProject[i].buildTime.day}`;
+  timeTag.innerHTML = `${dataProject[i].buildTime.day}/${dataProject[i].buildTime.month}/${dataProject[i].buildTime.year}`;
+  divMorPro.append(timeTag);
+
+  const divProSkill = document.createElement("div");
+  divProSkill.className = "project-skills";
+  divProSkill.setAttribute("data-project-skills", "");
+
+  for (let j = 0, len = dataProject[i].projectLanguages.length; j < len; j++) {
+    const spanTag = document.createElement("span");
+    spanTag.className = "btn-hov";
+    spanTag.innerText = dataProject[i].projectLanguages[j];
+    divProSkill.append(spanTag);
+  }
+
+  divMorPro.append(divProSkill);
+
+  projectList.append(li);
+}
+
+// sidebar variables
+
+const sidebar = document.getElementById("sidebar"),
+  sidebarBtnMore = document.getElementById("info-more-btn");
+
+// open and close window sidebar  for mobile
+
+sidebarBtnMore.addEventListener("click", function () {
+  toggelFunction(sidebar);
+});
+
+// var testimonial and  all item and bottom are here in testmonial
+
+const testimonialsItem = document.querySelectorAll(".testimonials__item"),
+  contentPouncing = document.querySelectorAll(".content__pouncing"),
+  cover = document.querySelectorAll(".cover"),
+  bottomClose = document.querySelectorAll(".element__pounce-close"),
+  // pounce var
+
+  avatarPounce = document.querySelector(".pounce__interface-avtar img"),
+  pounceTitle = document.querySelector(".pounce__title"),
+  pounceText = document.querySelector(".pounce__info-par p");
+
+// fetch all information when click in testimonail item and open window have this info
+
+testimonialsItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    avatarPounce.src = item.querySelector("[data-testimonial-avatar]").src;
+    avatarPounce.alt = item.querySelector("[data-testimonial-avatar]").alt;
+    pounceTitle.textContent = item.querySelector(
+      ".testimonial__text-title"
+    ).textContent;
+    pounceText.textContent = item.querySelector(
+      "[data-tasimonial-text-par]"
+    ).textContent;
+    toggelFunction(contentPouncing[0], cover[0]);
+  });
+});
+
+// remove class active => class window when it  open
+
+bottomClose[0].addEventListener("click", () => {
+  toggelFunction(contentPouncing[0], cover[0]);
+});
+
+// ranking project depend on box slect in mead small
+
+// var portfolio
+
+const selectBox = document.querySelector(".select__box-main"),
+  selectVAlue = document.querySelector(".select-value"),
+  selectItems = document.querySelectorAll(".select-item > button"),
+  projectItems = document.querySelectorAll("[data-type]");
+
+//for screen larg
+FilterItems = document.querySelectorAll(".select-item");
+
+selectBox.addEventListener("click", () => {
+  toggelFunction(selectBox);
+});
+
+selectItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    selectVAlue.innerHTML = item.textContent;
+    toggelFunction(selectBox);
+    filetrFunction(item.textContent.toLowerCase());
+  });
+});
+
+// for fliter element add and remove class active
+
+const filterItemsLarg = document.querySelectorAll(".filter-item");
+
+filterItemsLarg.forEach((item) => {
+  item.addEventListener("click", () => {
+    filetrFunction(item.firstElementChild.textContent.toLowerCase());
+    filterItemsLarg.forEach((item) =>
+      item.firstElementChild.classList.remove("active")
+    );
+    item.firstElementChild.classList.add("active");
+  });
+});
+
+//check form
+
+// contact form variables
+
+const form = document.querySelector(".form"),
+  formInputs = document.querySelectorAll("[data-set]"),
+  formBtn = document.querySelector(".form-btn");
+
+// add event to all form input field
+
+formInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+  });
+});
+
+//add event to
+
+let navigation = document.querySelectorAll("[data-nav]"),
+  allArticles = document.querySelectorAll("[data-article]");
+
+navigation.forEach((nav) => {
+  // remove class active from all nav
+  nav.addEventListener("click", () => {
+    navigation.forEach((nav) => {
+      nav.classList.remove("active");
+    });
+
+    // add class active on nav you ckilck and move to page
+
+    allArticles.forEach((article) => {
+      if (nav.dataset.nav === article.dataset.article) {
+        article.classList.add("active");
+
+        nav.classList.add("active");
+
+        scrollTo({ top: 0 });
+      } else {
+        article.classList.remove("active");
+      }
+    });
+  });
+});
+
+// fill span when scroll to skills and Lang
+
+const elmentsWantFill = document.querySelectorAll("[data-width]");
+
+const fillingWithScroll = function () {
+  elmentsWantFill.forEach((ele) => {
+    let trueOrFalse = ele.getBoundingClientRect().top - 40 < window.innerHeight;
+
+    if (trueOrFalse) ele.style.width = ele.dataset.width;
+    else ele.style.width = "0";
+  });
+};
+
+window.addEventListener("scroll", fillingWithScroll);
+////////////////////////////////////
+const languages = document.querySelectorAll("[data-fill]");
+
+const fillingCircle = function () {
+  languages.forEach((lan) => {
+    let trueOrFalse = lan.getBoundingClientRect().top + 20 < window.innerHeight;
+
+    if (trueOrFalse)
+      lan.querySelector("circle").style.strokeDashoffset = lan.dataset.fill;
+    else lan.querySelector("circle").style.strokeDashoffset = "690";
+  });
+};
+window.addEventListener("scroll", fillingCircle);
+
+////////////////////////////////////
+// add Information to moudel window
+
+const itemsProjects = document.querySelectorAll(".project__item "),
+  imgPounceProject = document.querySelector("[data-pounce-image]"),
+  titlePounceProject = document.querySelector("[data-pounce-title]"),
+  descPounceProject = document.querySelector("[data-pounce-desc]"),
+  linkPounceProject = document.querySelector("[data-projec-link]"),
+  timePounceProject = document.querySelector("[data-pounce-time]"),
+  langPounceProject = document.querySelector("[data-pounce-lang]");
+
+itemsProjects.forEach((project) => {
+  project.querySelector(".btn").addEventListener("click", () => {
+    imgPounceProject.src = project.querySelector("[data-project-image]").src;
+
+    imgPounceProject.alt = project.querySelector("[data-project-image]").alt;
+
+    titlePounceProject.innerHTML = project.querySelector(
+      "[data-project-title]"
+    ).innerHTML;
+
+    linkPounceProject.href = project.querySelector("[data-project-link]").href;
+
+    linkPounceProject.innerHTML = project.querySelector(
+      "[data-project-link]"
+    ).innerHTML;
+
+    timePounceProject.setAttribute(
+      "datetime",
+      project.querySelector("[data-project-time]").getAttribute("datetime")
+    );
+
+    timePounceProject.innerHTML = project.querySelector(
+      "[data-project-time]"
+    ).innerHTML;
+
+    langPounceProject.innerHTML = project.querySelector(
+      "[data-project-skills]"
+    ).innerHTML;
+
+    toggelFunction(contentPouncing[1], cover[1]);
+  });
+});
+
+bottomClose[1].addEventListener("click", () => {
+  toggelFunction(contentPouncing[1], cover[1]);
+});
+
+// add event and efect to mouse
+
+const cursorOutline = document.getElementById("cursor-outline"),
+  container = document.querySelector(".container");
+
+document.body.addEventListener("mousemove", (event) => {
+  if (event.srcElement.classList.contains("showbigger")) {
+    cursorOutline.classList.add("biggerThan");
+  } else {
+    cursorOutline.classList.remove("biggerThan");
+  }
+  const postionX = event.clientX;
+  const postionY = event.clientY;
+
+  cursorOutline.style.left = `${postionX}px`;
+
+  cursorOutline.style.top = `${postionY}px`;
+
+  cursorOutline.animate(
+    {
+      left: `${postionX}px`,
+      top: `${postionY}px`,
+    },
+    { duration: 500, fill: "forwards" }
+  );
+});
+
+//functions : >>>--|
+
+// Functin add and remove class active
+
+const toggelFunction = (...eles) => {
+  eles.forEach((ele) => {
+    ele.classList.toggle("active");
+  });
+};
+
+// function filter project  hide and show project as same title you slected
+
+function filetrFunction(selected) {
+  projectItems.forEach((item) => {
+    if (selected === "all") {
+      item.classList.add("active");
+    } else if (selected === item.dataset.type) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  });
+}
